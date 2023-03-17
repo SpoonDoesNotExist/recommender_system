7944f96780c51182b1781b82b11f865d4cac08e3
eduard <ed.khusnutdinow@yandex.ru>
2023-03-17 13:15:36 +0700

    Добавил waitress WSGI для production режима Добавил waitress в requirements.txt

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
 from application.app import app
+from waitress import serve
+
+run_mode = 'prod'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    if run_mode == 'dev':
+        app.run(host='0.0.0.0', port=5000)
+    elif run_mode == 'prod':
+        serve(app, host='0.0.0.0', port=5000)
+    else:
+        raise NotImplementedError(f'Unexpected run mode: {run_mode}')
